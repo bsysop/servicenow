19379abeb4c23716a71735d86ea4919a61912e61
Michael Havas <michael.havas@gmail.com>
2023-11-16 19:40:42 +0000

    Enabling table file and username/password authentication

--- servicescan.py
+++ servicescan.py
@@ -5,11 +5,12 @@ import argparse
 import requests
 import json
 import re
+import getpass
 
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 
-def check_vulnerability(url, g_ck_value, cookies, s, proxies, fast_check, display):
+def check_vulnerability(url, g_ck_value, cookies, s, proxies, fast_check, display, tables):
     table_list = [
         "t=cmdb_model&f=name",
         "t=cmn_department&f=app_name",
@@ -39,6 +40,8 @@ def check_vulnerability(url, g_ck_value, cookies, s, proxies, fast_check, displa
         "t=sys_user&f=name",
         "t=customer_contact&f=name"
     ]
+    if tables is not None:
+        table_list = tables
 
     if fast_check:
         table_list = ["t=kb_knowledge"]
@@ -67,25 +70,23 @@ def check_vulnerability(url, g_ck_value, cookies, s, proxies, fast_check, displa
                 if 'data' in response_json['result']:
                     if 'count' in response_json['result']['data'] and response_json['result']['data']['count'] > 0:
                         if response_json['result']['data']['list'] and len(response_json['result']['data']['list']) > 0:
-                            if response_json['result']['data']['list'] and len(response_json['result']['data']['list']) > 0:
-                                if any(d['display_field']['display_value'] is not None for d in response_json['result']['data']['list']):
-                                    print(f"{post_url} is EXPOSED, and REALLY LEAKING data. Check ACLs ASAP.")
-                                else:
-                                    print(f"{post_url} is EXPOSED, and POTENTIALLY LEAKING data. Check ACLs ASAP.")
-                                
-                                if display:
-                                    try:
-                                        items = response_json['result']['data']['list']
-                                        for item in items:
-                                            display_value = item['display_field']['display_value']
-                                            sys_id = item['sys_id']                                        
-                                            if "sys_attachment" in table:
-                                                print(f'{url}/sys_attachment.do?sys_id={sys_id}#{display_value}')
-                                            else:
-                                                print(f'{display_value}')
-                                        print("")
-                                    except:
-                                        print('failed to extract display data')
+                            if any(d['display_field']['display_value'] is not None for d in response_json['result']['data']['list']):
+                                print(f"{post_url} is EXPOSED, and REALLY LEAKING data. Check ACLs ASAP.")
+                            else:
+                                print(f"{post_url} is EXPOSED, and POTENTIALLY LEAKING data. Check ACLs ASAP.")
+                            if display:
+                                try:
+                                    items = response_json['result']['data']['list']
+                                    for item in items:
+                                        display_value = item['display_field']['display_value']
+                                        sys_id = item['sys_id']                                        
+                                        if "sys_attachment" in table:
+                                            print(f'{url}/sys_attachment.do?sys_id={sys_id}#{display_value}')
+                                        else:
+                                            print(f'{display_value}')
+                                    print("")
+                                except:
+                                    print('failed to extract display data')
                         else:
                             print(f"{post_url} is EXPOSED, but data is NOT leaking likely because ACLs are blocking. Mark Widgets as not Public.")
                         vulnerable_urls.append(post_url)
@@ -93,9 +94,20 @@ def check_vulnerability(url, g_ck_value, cookies, s, proxies, fast_check, displa
     return vulnerable_urls
 
 
-def check_url_get_headers(url, proxies):
+def check_url_get_headers(url, proxies, username, password):
     # get the session
+    url = url.strip()
+    url = url.rstrip('/')
+
     s = requests.Session()
+    if username is not None:
+        login_url = f"{url}/login.do"
+        payload = {
+            'user_name': username,
+            'user_password': password,
+            'sys_action': 'sysverb_login'
+        }
+        s.post(login_url, payload, proxies=proxies)
     response = s.get(url, verify=False, proxies=proxies)
     cookies = s.cookies.get_dict()
 
@@ -110,7 +122,7 @@ def check_url_get_headers(url, proxies):
         return None, cookies, s
 
 
-def main(url, fast_check, proxy, display, headers):
+def main(url, fast_check, proxy, display, headers, tables, username, password):
     if proxy:
         proxies = {'http': proxy, 'https': proxy}
     else:
@@ -118,11 +130,11 @@ def main(url, fast_check, proxy, display, headers):
 
     url = url.strip()
     url = url.rstrip('/')
-    g_ck_value, cookies, s = check_url_get_headers(url, proxies)
+    g_ck_value, cookies, s = check_url_get_headers(url, proxies, username, password)
     if g_ck_value is None:
         print(f"{url} has no g_ck. Continuing test without X-UserToken header")
 
-    vulnerable_url = check_vulnerability(url, g_ck_value, cookies, s, proxies, fast_check, display)
+    vulnerable_url = check_vulnerability(url, g_ck_value, cookies, s, proxies, fast_check, display, tables)
     if vulnerable_url and headers :
         print("Headers to forge requests:")
         if g_ck_value is not None:
@@ -143,20 +155,43 @@ if __name__ == '__main__':
     parser.add_argument('--proxy', help='Proxy server in the format http://host:port', default=None)
     parser.add_argument('--display', action='store_true', help='output display name for quick visual')
     parser.add_argument('--headers', action='store_true', help='print headers to forge request with any vulnerable systems')
+    parser.add_argument('--table-file', help='The specific tables to test')
+    parser.add_argument('--username', help='The username to login with')
+    parser.add_argument('--password', help='The password to login with')
     args = parser.parse_args()
     fast_check = args.fast_check
     display = args.display
     proxy = args.proxy
     headers = args.headers
+    table_file = args.table_file
+    username = args.username
+    password = args.password
+
+    # Prompt for a password if required
+    if username is not None and password is None:
+        password = getpass.getpass("Enter password: ")
+
+    # Collect tables to test from file if required
+    tables = None
+    if table_file is not None:
+        try:
+            table_file = args.table_file
+            with open(table_file, 'r') as file:
+                tables = file.read().splitlines()
+        except FileNotFoundError:
+            print(f"Could not find {url_file}")
+        except Exception as e:
+            print(f"Error occurred: {e}")
+
     if args.url:
-        any_vulnerable = main(args.url, fast_check, proxy, display, headers)
+        any_vulnerable = main(args.url, fast_check, proxy, display, headers, tables, username, password)
     else:
         try:
             url_file = args.file
             with open(url_file, 'r') as file:
                 url_list = file.readlines()
             for url in url_list:
-                if main(url, fast_check, proxy, display, headers):
+                if main(url, fast_check, proxy, display, headers, tables, username, password):
                     any_vulnerable = True  # At least one URL was vulnerable
         except FileNotFoundError:
             print(f"Could not find {url_file}")
